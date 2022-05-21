67a5bc18df90f007785c7ac1b9e4b0cc0d98d0f4
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-05-20 20:54:39 -0400

    finsihed the intern test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -4,3 +4,21 @@ const Intern = require('../lib/Intern');
 test('create an intern object', () => {
     const intern = new Intern('Khloe')
 });
+
+test('set school with constructor', () => {
+    const testValue = 'Spelman';
+    const e = new Intern('Khloe', 1, 'intern@workemail.com', testValue);
+    expect(e.school).toBe(testValue);
+});
+
+test('get school with getSchool method', () => {
+    const testValue = 'Spelman';
+    const e = new Intern('Khloe', 1, 'intern@workemail.com', testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
+
+test('getRole return Intern', () =>{
+    const testValue = 'Intern';
+    const e = new Intern('Khloe', 1, 'intern@workemail.com'. testValue);
+    expect(e.getRole()).toBe(testValue);
+});
