d286c808aee08d6a8483028caf96ea0cd31c11c9
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-05-20 20:43:49 -0400

    finished test for Employee and Engineer

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,30 @@
 const Employee = require('../lib/Employee');
 
+//new employee
 test('create an employee object', () => {
     const employee = new Employee ('Diana');
+});
+
+//ID
+test('set id with constructor', () => {
+    const testValue = 100;
+    const e = new Employee('Aisha', testValue);
+    
+    expect(e.id).toBe(testValue);
+});
+
+
+//Email
+test('set email with constructor', () => {
+    const testValue = 'employee@workemail.com'
+    const e = new Employee('Aisha', 1, testValue);
+
+    expect(e.email).toBe(testValue);
+});
+
+
+//Gets role to ensure "Employee"
+test('getRole return Employee', () => {
+    const testValue = 'Employee';
+    const e = new Employee('Diana', 1, 'employee@workemail.com');
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,6 +1,23 @@
 const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
 
+
+//Engineer
 test('creates an engineer object', () => {
     const engineer = new Engineer('Samuel');
+});
+
+
+//Github
+test('set github accoint with constructor', () => {
+const testValue = 'GitHubAccount';
+const e = new Engineer('Lulu', 1 , 'engineer@workemail.com', testValue);
+expect(e.github).toBe(testValue);
+});
+
+//getRole
+test('getRole value to be Engineer', () => {
+    const testValue = "Engineer";
+    const e = new Engineer('Lulu', 1, 'engineer@workemail.com', testValue);
+    expect(e.getGitHub()).toBe(testValue);
 });
