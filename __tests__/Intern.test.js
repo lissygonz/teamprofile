87403b251949379106169153065fce18a5f0b1c4
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-05-20 21:04:49 -0400

    finished adding all tests for Intern and Manager

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,22 +1,25 @@
 const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern');
 
+//Intern
 test('create an intern object', () => {
     const intern = new Intern('Khloe')
 });
 
+//School
 test('set school with constructor', () => {
     const testValue = 'Spelman';
     const e = new Intern('Khloe', 1, 'intern@workemail.com', testValue);
     expect(e.school).toBe(testValue);
 });
 
+//Get School
 test('get school with getSchool method', () => {
     const testValue = 'Spelman';
     const e = new Intern('Khloe', 1, 'intern@workemail.com', testValue);
     expect(e.getSchool()).toBe(testValue);
 });
-
+//getRole Intern
 test('getRole return Intern', () =>{
     const testValue = 'Intern';
     const e = new Intern('Khloe', 1, 'intern@workemail.com'. testValue);
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,30 @@
 const Employee = require('../lib/Employee');
 const Manager = require('../lib/Manager')
 
+
+//Manager
 test('creates a manager object', () => {
     const manager = new Manager('Jose')
+});
+
+//OfficeNumber
+test('set office number with constructor', () => {
+    const testValue = 100;
+    const e = new Manager('Jose', 1, 'manager@workemail.com', testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+
+
+//OfficeNumber method
+test('get office Number with getOfficeNumber method', () => {
+    const testValue = 100;
+    const e = new Manager('Jose', 1, 'manager@workemail.com', testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+});
+
+//getRole manager
+test('getRole return Manager', () => {
+    const testValue = 'Manager';
+    const e = new Manager('Jose', 1, 'manager@workemail.com', 100);
+    expect(e.getRole()).toBe(testValue);
 });
