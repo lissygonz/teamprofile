294002a297d98ec9bd812de878c2f4f729aaed44
lissygonz <99147859+lissygonz@users.noreply.github.com>
2022-05-21 12:10:12 -0400

    added the dependencies and constructors to index

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello Node")
+// the Dependencies
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const jest = require('jest');
+
+//Constructors
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
